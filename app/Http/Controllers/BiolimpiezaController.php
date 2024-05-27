a214a831714bd4b7cd0ed6b39a1e5f1b78221ce4
federico-lopez-itti <federico.lopez@itti.digital>
2024-05-27 14:47:19 -0400

    se cambia el controlador

--- app/Http/Controllers/BiolimpiezaController.php
+++ app/Http/Controllers/BiolimpiezaController.php
@@ -3,35 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Mail\BudgetMailable;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class BiolimpiezaController extends Controller
 {
     public function sendBudget(Request $request){
-        $this->sendMail($request->all(), 'SOLICITAR PRESUPUESTO');
-        return response()->json(['message' => 'Correo enviado']);
+        $send = $this->sendMail($request->all(), 'SOLICITAR PRESUPUESTO');
+        return response()->json($send);
     }
 
     public function sendPlan(Request $request){
-        $this->sendMail($request->all(), 'SOLICITAR PLAN');
-        return response()->json(['message' => 'Correo enviado']);
+        $send = $this->sendMail($request->all(), 'SOLICITAR PLAN');
+        return response()->json($send);
     }
 
     public function sendQuestion(Request $request){
-        $this->sendMail($request->all(), 'PREGUNTAS FRECUENTES');
-        return response()->json(['message' => 'Correo enviado']);
+        $send = $this->sendMail($request->all(), 'PREGUNTAS FRECUENTES');
+        return response()->json($send);
     }
 
     public function sendContact(Request $request){
-        $this->sendMail($request->all(), 'CONTACTO');
-        return response()->json(['message' => 'Correo enviado']);
+        $send = $this->sendMail($request->all(), 'CONTACTO');
+        return response()->json($send);
     }
 
-    private function sendMail(array $data, string $subject){
-        $data['subject'] = $subject;
-        $mail = new BudgetMailable($data, $subject);	  
-        Mail::to(['biolimpiezapy@gmail.com', 'vazquez182@gmail.com'])->send($mail);
+    private function sendMail(array $data, string $subject)
+    {
+        try{
+            $data['subject'] = $subject;
+            $mail            = new BudgetMailable($data, $subject);
+            Mail::to(['biolimpiezapy@gmail.com', 'vazquez182@gmail.com'])->send($mail);
+            return true;
+            
+        }catch(Exception $ex){
+            Log::error(__METHOD__, [ $ex->getMessage(), $data ]);
+            return false;
+        }
+    
     }
 }
 
